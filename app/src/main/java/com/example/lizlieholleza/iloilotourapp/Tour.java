2aece3474be9decc7cb4c893ae8c6ffa4c8433b1
Liz <lizlie.holleza@awsys-i.com>
2018-07-17 13:58:11 +0800

    create tour class and add attr with getters and its costructors

---
+++ app/src/main/java/com/example/lizlieholleza/iloilotourapp/Tour.java
@@ -0,0 +1,75 @@
+package com.example.lizlieholleza.iloilotourapp;
+
+public class Tour {
+    private String name;
+    private int imageID;
+    private int numberOfTours;
+    private float distance;
+    private float time;
+    private String food;
+    private int soundID;
+    private String englishWord;
+    private String nativeWord;
+
+    public Tour(String place, int tours, int image) {
+        this.name = place;
+        this.numberOfTours = tours;
+        this.imageID = image;
+    }
+
+    public Tour(String place, float distance, float time, int image) {
+        this.name = place;
+        this.distance = distance;
+        this.time = time;
+        this.imageID = image;
+    }
+
+    public Tour(String event, int image) {
+        this.name = event;
+        this.imageID= image;
+    }
+
+    public Tour(String place, String food, int image) {
+        this.name = place;
+        this.food = food;
+        this.imageID = image;
+    }
+    public Tour(String englishWord, String nativeWord, int sound, int image) {
+        this.englishWord = englishWord;
+        this.nativeWord = nativeWord;
+        this.soundID = sound;
+        this.imageID = image;
+    }
+
+    public String getName() {
+        return name;
+    };
+
+    public int getImageID() {
+        return imageID;
+    }
+
+    public int getNumberOfTours() {
+        return numberOfTours;
+    }
+
+    public float getDistance() {
+        return distance;
+    }
+
+    public float getTime() {
+        return time;
+    }
+
+    public int getSoundID() {
+        return soundID;
+    }
+
+    public String getEnglishWord() {
+        return englishWord;
+    }
+
+    public String getNativeWord() {
+        return nativeWord;
+    }
+}
