feb9c76c49b5d2d09fe4fb2ba695468e24f18e10
Liz <lizlie.holleza@awsys-i.com>
2018-07-18 18:46:07 +0800

    create raw directory in resource folder and add items in the arraylist

--- app/src/main/java/com/example/lizlieholleza/iloilotourapp/LanguageFragment.java
+++ app/src/main/java/com/example/lizlieholleza/iloilotourapp/LanguageFragment.java
@@ -15,6 +15,14 @@ public class LanguageFragment extends Fragment {
     public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.item_list, container, false);
         final ArrayList<Tour> language = new ArrayList<Tour>();
-        language.add(new Tour("Beautiful", "Gwapa", ))
+        language.add(new Tour("Beautiful1", "Gwapa", R.raw.family_older_sister, R.drawable.miagao));
+        language.add(new Tour("Beautiful2", "Gwapa", R.raw.family_older_sister, R.drawable.miagao));
+        language.add(new Tour("Beautiful3", "Gwapa", R.raw.family_older_sister, R.drawable.miagao));
+        language.add(new Tour("Beautiful4", "Gwapa", R.raw.family_older_sister, R.drawable.miagao));
+        language.add(new Tour("Beautiful5", "Gwapa", R.raw.family_older_sister, R.drawable.miagao));
+        language.add(new Tour("Beautiful6", "Gwapa", R.raw.family_older_sister, R.drawable.miagao));
+        language.add(new Tour("Beautiful7", "Gwapa", R.raw.family_older_sister, R.drawable.miagao));
+        language.add(new Tour("Beautiful8", "Gwapa", R.raw.family_older_sister, R.drawable.miagao));
+
     }
 }
