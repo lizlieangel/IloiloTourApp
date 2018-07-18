6f27cdcac06e63b3953e9b20d3a28c5102336a33
Liz <lizlie.holleza@awsys-i.com>
2018-07-18 13:25:45 +0800

    Updated the place fragment

--- app/src/main/java/com/example/lizlieholleza/iloilotourapp/PlaceFragment.java
+++ app/src/main/java/com/example/lizlieholleza/iloilotourapp/PlaceFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -15,6 +16,16 @@ public class PlaceFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.item_list, container, false);
         final ArrayList<Tour> placeList = new ArrayList<Tour>();
-        placeList.add(new Tour("Oton",))
+        placeList.add(new Tour("Miagao", 19, 2,R.drawable.miagao));
+        placeList.add(new Tour("Miagao1", 12, 2,R.drawable.miagao));
+        placeList.add(new Tour("Miagao2", 17, 2,R.drawable.miagao));
+        placeList.add(new Tour("Miagao3", 18, 2,R.drawable.miagao));
+        placeList.add(new Tour("Miagao4", 15, 2,R.drawable.miagao));
+        placeList.add(new Tour("Miagao5", 10, 2,R.drawable.miagao));
+
+        PlaceAdapter adapter = new PlaceAdapter(getActivity(), placeList);
+        ListView lv = (ListView) rootview.findViewById(R.id.item_list);
+        lv.setAdapter(adapter);
+        return rootview;
     }
 }
