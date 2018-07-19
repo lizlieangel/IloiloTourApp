97061d737719ee4035188aa9903a3ef020488656
Liz <lizlie.holleza@awsys-i.com>
2018-07-19 18:25:44 +0800

    update the value in the arraylist

--- app/src/main/java/com/example/lizlieholleza/iloilotourapp/FoodFragment.java
+++ app/src/main/java/com/example/lizlieholleza/iloilotourapp/FoodFragment.java
@@ -13,14 +13,14 @@ public class FoodFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.item_list, container, false);
         final ArrayList<Tour> food = new ArrayList<Tour>();
-        food.add(new Tour("Miagao1", "Ginamos1", "namit gid ya dan, plus epares sa manga", R.drawable.miagao));
-        food.add(new Tour("Miagao2", "Ginamos2", "namit gid ya dan, plus epares sa manga", R.drawable.miagao));
-        food.add(new Tour("Miagao3", "Ginamos3", "namit gid ya dan, plus epares sa manga", R.drawable.miagao));
-        food.add(new Tour("Miagao4", "Ginamos4", "namit gid ya dan, plus epares sa manga", R.drawable.miagao));
-        food.add(new Tour("Miagao5", "Ginamos5", "namit gid ya dan, plus epares sa manga", R.drawable.miagao));
-        food.add(new Tour("Miagao6", "Ginamos6", "namit gid ya dan, plus epares sa manga", R.drawable.miagao));
-        food.add(new Tour("Miagao7", "Ginamos7", "namit gid ya dan, plus epares sa manga", R.drawable.miagao));
-        food.add(new Tour("Miagao8", "Ginamos8", "namit gid ya dan, plus epares sa manga", R.drawable.miagao));
+        food.add(new Tour("Lapaz", "Lapaz Batchoy", "Batchoy is a noodle soup made with pork offal, crushed pork cracklings, chicken stock, beef loin and round noodles.\n" +
+                "Its origins can be traced to the district of La Paz, Iloilo City in the Philippines, \n" +
+                "hence it is often referred to as La Paz Batchoy", R.drawable.batchoy));
+        food.add(new Tour("Iloilo City", "Biscocho Haus", "Biscocho Haus has been serving Ilonggos and tourists visiting the city for more than three decades now. ", R.drawable.biscocho));
+        food.add(new Tour("Villa", "Breakthrough", "Extraordinary seafood specialties and a bountiful array of different cuisines anchor an eclectic menu.", R.drawable.breakthrough));
+        food.add(new Tour("Lapaz", "Madge", "Madge Café is one of the most jam-packed coffee place in Iloilo City.", R.drawable.madge));
+        food.add(new Tour("Iloilo City", "Robertos", "Roberto's siopao might be the perfect pasalubong for your loved ones. ", R.drawable.robertos));
+        food.add(new Tour("Villa", "Tatoy's", "If you are into seafood and native Filipino cuisine. ", R.drawable.tatoys));
 
         FoodAdapter adapter = new FoodAdapter(getActivity(),food);
         ListView lv = (ListView) rootview.findViewById(R.id.item_list);
