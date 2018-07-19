d1ca42eaa3b20363ce495c80df0fb538c4a4a7ad
Liz <lizlie.holleza@awsys-i.com>
2018-07-19 11:02:24 +0800

    updated the info inside the arraylist: add another parameter

--- app/src/main/java/com/example/lizlieholleza/iloilotourapp/FoodFragment.java
+++ app/src/main/java/com/example/lizlieholleza/iloilotourapp/FoodFragment.java
@@ -13,14 +13,14 @@ public class FoodFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.item_list, container, false);
         final ArrayList<Tour> food = new ArrayList<Tour>();
-        food.add(new Tour("Miagao1", "Ginamos1 namit gid ya dan, plus epares sa manga", R.drawable.miagao));
-        food.add(new Tour("Miagao2", "Ginamos2 namit gid ya dan, plus epares sa manga", R.drawable.miagao));
-        food.add(new Tour("Miagao3", "Ginamos3 namit gid ya dan, plus epares sa manga", R.drawable.miagao));
-        food.add(new Tour("Miagao4", "Ginamos4 namit gid ya dan, plus epares sa manga", R.drawable.miagao));
-        food.add(new Tour("Miagao5", "Ginamos5 namit gid ya dan, plus epares sa manga", R.drawable.miagao));
-        food.add(new Tour("Miagao6", "Ginamos6 namit gid ya dan, plus epares sa manga", R.drawable.miagao));
-        food.add(new Tour("Miagao7", "Ginamos7 namit gid ya dan, plus epares sa manga", R.drawable.miagao));
-        food.add(new Tour("Miagao8", "Ginamos8 namit gid ya dan, plus epares sa manga", R.drawable.miagao));
+        food.add(new Tour("Miagao1", "Ginamos1", namit gid ya dan, plus epares sa manga", R.drawable.miagao));
+        food.add(new Tour("Miagao2", "Ginamos2", namit gid ya dan, plus epares sa manga", R.drawable.miagao));
+        food.add(new Tour("Miagao3", "Ginamos3", namit gid ya dan, plus epares sa manga", R.drawable.miagao));
+        food.add(new Tour("Miagao4", "Ginamos4", namit gid ya dan, plus epares sa manga", R.drawable.miagao));
+        food.add(new Tour("Miagao5", "Ginamos5", namit gid ya dan, plus epares sa manga", R.drawable.miagao));
+        food.add(new Tour("Miagao6", "Ginamos6", namit gid ya dan, plus epares sa manga", R.drawable.miagao));
+        food.add(new Tour("Miagao7", "Ginamos7", namit gid ya dan, plus epares sa manga", R.drawable.miagao));
+        food.add(new Tour("Miagao8", "Ginamos8", namit gid ya dan, plus epares sa manga", R.drawable.miagao));
 
         FoodAdapter adapter = new FoodAdapter(getActivity(),food);
         ListView lv = (ListView) rootview.findViewById(R.id.item_list);
