144cefe5535de0fdf92bdc8d312fcb64365ba823
Lizlie <lizlieholleza@gmail.com>
2018-07-21 17:52:46 +0800

    create place activity class

---
+++ app/src/main/java/com/example/lizlieholleza/iloilotourapp/PlaceActivity.java
@@ -0,0 +1,19 @@
+package com.example.lizlieholleza.iloilotourapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class PlaceActivity extends AppCompatActivity{
+    private ImageView place_image;
+    private TextView place
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.information);
+    }
+
+
+}
